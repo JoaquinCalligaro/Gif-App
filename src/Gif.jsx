ad2c8f7ead71df3a2f69d591c83b6523a9b268fa
Joaquin Calligaro <joaquincalligaro@gmail.com>
2025-08-02 05:19:26 -0300

    feat: add BackgroundGradientAnimation component, enhance styling, and integrate new animations

--- src/Gif.jsx
+++ src/Gif.jsx
@@ -1,7 +1,7 @@
 export const Gif = ({ gif }) => {
   return (
     <img
-      className="w-4/5 h-52 rounded-lg object-cover"
+      className="w-4/5 h-62 rounded-lg object-cover  animate-zoom-in duration-1000 delay-1000"
       key={gif.id}
       src={gif.images.original.url}
       alt={gif.title}
--- src/GridGifs.jsx
+++ src/GridGifs.jsx
@@ -1,7 +1,7 @@
 import { Gif } from './Gif';
 export const GridGifs = ({ gifs }) => {
   return (
-    <div className="grid grid-cols-3 gap-11 w-full p-5">
+    <div className="grid grid-cols-3 gap-11 w-full p-5 animate-zoom-in duration-1000 delay-1000">
       {gifs.map((gif) => (
         <Gif key={gif.id} gif={gif} />
       ))}
