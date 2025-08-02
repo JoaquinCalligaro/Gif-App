394b14bc62229251e43b88e6607ac61b23d54e88
Joaquin Calligaro <joaquincalligaro@gmail.com>
2025-08-02 06:34:37 -0300

    feat: restore cleaned App.jsx with .env usage

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { Search } from './Search';
+import { GridGifs } from './GridGifs';
+import { BackgroundGradientAnimation } from '../src/Background';
+function App() {
+  //Estado
+  const [valorInput, setValorInput] = useState('');
+  const [gifs, setGifs] = useState([]);
+
+  const onChange = (evento) => {
+    const valor = evento.target.value;
+    setValorInput(valor);
+  };
+
+  const getFigs = async (query) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${
+      import.meta.env.VITE_GIPHY_API_KEY
+    }&q=${query}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.data;
+  };
+
+  const onSubmit = async (evento) => {
+    evento.preventDefault();
+    const gifs = await getFigs(valorInput);
+    console.log(gifs);
+    setGifs(gifs);
+  };
+
+  getFigs();
+  return (
+    <div className="min-h-screen relative">
+      <BackgroundGradientAnimation />
+      <Search valorInput={valorInput} onChange={onChange} onSubmit={onSubmit} />
+      <GridGifs gifs={gifs} />
+    </div>
+  );
+}
+export default App;
