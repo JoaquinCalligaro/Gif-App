8c68562bcd93fc0c4cffd1ad37328f5175809526
Joaquin Calligaro <joaquincalligaro@gmail.com>
2025-08-03 02:43:01 -0300

    fix: add justify-items-center to GridGifs layout for better alignment

--- src/GridGifs.jsx
+++ src/GridGifs.jsx
@@ -2,7 +2,7 @@ import { Gif } from './Gif';
 export const GridGifs = ({ gifs }) => {
   return (
     <div
-      className="grid grid-cols-1 
+      className="grid grid-cols-1 justify-items-center
       md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-15 w-full p-5">
       {gifs.map((gif, index) => (
         <Gif key={gif.id} gif={gif} index={index} />
