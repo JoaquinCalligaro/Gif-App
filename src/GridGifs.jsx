1501ea7b913743d462fdd7f785d2077dad1128f6
Joaquin Calligaro <joaquincalligaro@gmail.com>
2025-08-03 02:37:51 -0300

    fix: update GridGifs layout for improved responsiveness and spacing

--- src/GridGifs.jsx
+++ src/GridGifs.jsx
@@ -1,7 +1,9 @@
 import { Gif } from './Gif';
 export const GridGifs = ({ gifs }) => {
   return (
-    <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-11 w-full p-5">
+    <div
+      className="grid grid-cols-2 
+      md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-6 w-full p-5">
       {gifs.map((gif, index) => (
         <Gif key={gif.id} gif={gif} index={index} />
       ))}
