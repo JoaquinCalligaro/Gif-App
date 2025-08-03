79e05daa6274608830c02db3e67cec5f99021957
Joaquin Calligaro <joaquincalligaro@gmail.com>
2025-08-03 02:26:03 -0300

    fix: update GridGifs layout to use responsive grid columns

--- src/GridGifs.jsx
+++ src/GridGifs.jsx
@@ -1,7 +1,7 @@
 import { Gif } from './Gif';
 export const GridGifs = ({ gifs }) => {
   return (
-    <div className="grid grid-cols-3 gap-11 w-full p-5">
+    <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-11 w-full p-5">
       {gifs.map((gif, index) => (
         <Gif key={gif.id} gif={gif} index={index} />
       ))}
