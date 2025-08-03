46e50c37fc837927e91e916f220076c31322e1d8
Joaquin Calligaro <joaquincalligaro@gmail.com>
2025-08-03 02:40:54 -0300

    feat: bugs fixed

--- src/GridGifs.jsx
+++ src/GridGifs.jsx
@@ -2,8 +2,8 @@ import { Gif } from './Gif';
 export const GridGifs = ({ gifs }) => {
   return (
     <div
-      className="grid grid-cols-2 
-      md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-6 w-full p-5">
+      className="grid grid-cols-1 
+      md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-15 w-full p-5">
       {gifs.map((gif, index) => (
         <Gif key={gif.id} gif={gif} index={index} />
       ))}
